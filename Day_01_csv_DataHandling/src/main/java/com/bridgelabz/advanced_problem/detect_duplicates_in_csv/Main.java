679d48ab851a47f0be093cdc0bc26ab82422ac00
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-18 23:02:09 +0530

    [Arpita]Add. detect_duplicates_in_csv problem

---
+++ Day_01_csv_DataHandling/src/main/java/com/bridgelabz/advanced_problem/detect_duplicates_in_csv/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.advanced_problems.detect_duplicates_in_csv;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "src/main/resources/students_duplicate.csv";
+        com.bridgelabz.advanced_problems.detect_duplicates_in_csv.DetectDuplicates.findDuplicates(filePath);
+    }
+}
