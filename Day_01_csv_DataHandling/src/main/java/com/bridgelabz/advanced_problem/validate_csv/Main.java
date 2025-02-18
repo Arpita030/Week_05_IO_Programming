6c5f1df989bf24a51e055fc086db20ba9b8cd91b
Arpita030 <0111as211013.arpita@gmail.com>
2025-02-18 23:07:04 +0530

    [Arpita]Add. validate_csv problem

---
+++ Day_01_csv_DataHandling/src/main/java/com/bridgelabz/advanced_problem/validate_csv/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.advanced_problems.validate_csv;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "src/main/resources/employees.csv";
+        com.bridgelabz.advanced_problems.validate_csv.ValidateCSV.validateCSV(filePath);
+    }
+}
